4fff3d131518be917867709de647c23a70356056
Kuba Karpierz <mr.karpierz@gmail.com>
2024-01-22 19:59:14 -0500

    undid

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
-import { createServer } from "node:http.js";
-import { fileURLToPath } from "node:url.js";
-import { dirname, join } from "node:path.js";
+import { createServer } from "node:http";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
 import { Server } from "socket.io";
 import { v4 as uuidv4 } from "uuid";
 
